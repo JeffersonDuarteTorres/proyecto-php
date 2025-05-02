e92451ea97118da6571bb9164bf2e16c69a8589c
JeynerCastañeda <8a.castaneda.jeyner.09@gmail.com>
2025-05-02 15:35:03 -0500

    Funciona Agregar Registro

--- modelos/registro.modelo.php
+++ modelos/registro.modelo.php
@@ -9,7 +9,8 @@ class ModeloRegistro {
     =============================================*/
     static public function mdlRegistro($tabla, $datos){
         
-        $sql = "INSERT INTO {$tabla} (pers_nombre, pers_telefono, pers_correo, pers_clave) VALUES (:nombre, :telefono, :correo, :clave)";
+        $sql = "INSERT INTO {$tabla} (pers_nombre, pers_telefono, pers_correo, pers_clave)
+         VALUES (:nombre, :telefono, :correo, :clave)";
 
         $stmt = Conexion::conectar()->prepare($sql);
 
